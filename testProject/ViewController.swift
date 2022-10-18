9812e868e82159c6d94445f3f03d2a46aac8e80a
Stasevich <sergei.stasevich@inbox.ru>
2022-10-18 21:40:40 +0200

    creat Buttons

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -4,6 +4,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelGame: UILabel!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
    
